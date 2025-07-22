21491718f0dfc9b8b8d40f2f50ea68bc85387cdc
varinder.rai <varinder.rai@netsmartz.com>
2025-07-22 19:30:46 +0530

    Fixed an issue

--- PRReviewProject/PRReviewProject/Pages/Product/Index.cshtml.cs
+++ PRReviewProject/PRReviewProject/Pages/Product/Index.cshtml.cs
@@ -9,7 +9,7 @@ namespace PRReviewProject.Pages.Product
         public void OnGet()
         {
             //int nu = Convert.ToInt32("abc");
-            int nu = Convert.ToInt32
+            int nu = Convert.ToInt32();
             ProductList = new List<string> { "LapTop", "Mobile", "PC", "PS" };
         }
     }
