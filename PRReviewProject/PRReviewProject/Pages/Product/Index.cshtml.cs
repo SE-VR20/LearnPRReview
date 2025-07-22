9910a86d72d80e875998251279d5cec7972b4e2c
SE-VR20 <varinder.rai@netsmartz.com>
2025-07-22 19:12:55 +0530

    Merge pull request #1 from SE-VR20/VR-PrivacyPage
    Added Privacy and the Home Page navigation links

---
+++ PRReviewProject/PRReviewProject/Pages/Product/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRReviewProject.Pages.Product
+{
+    public class IndexModel : PageModel
+    {
+        public List<string> ProductList { get; set; }
+        public void OnGet()
+        {
+            ProductList = new List<string> { "LapTop", "Mobile", "PC", "PS" };
+        }
+    }
+}
