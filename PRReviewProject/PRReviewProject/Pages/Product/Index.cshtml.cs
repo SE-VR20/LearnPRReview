e539e3aa61accb22a53dce0b3aa57838b19dc077
varinder.rai <varinder.rai@netsmartz.com>
2025-07-22 19:26:14 +0530

    Update Index.cshtml.cs with debugging comment
    Added a commented line for converting a string to an integer, which may assist in debugging. Adjusted the initialization of the `ProductList` for proper syntax.

--- PRReviewProject/PRReviewProject/Pages/Product/Index.cshtml.cs
+++ PRReviewProject/PRReviewProject/Pages/Product/Index.cshtml.cs
@@ -8,6 +8,8 @@ namespace PRReviewProject.Pages.Product
         public List<string> ProductList { get; set; }
         public void OnGet()
         {
+            //int nu = Convert.ToInt32("abc");
+            int nu = Convert.ToInt32
             ProductList = new List<string> { "LapTop", "Mobile", "PC", "PS" };
         }
     }
