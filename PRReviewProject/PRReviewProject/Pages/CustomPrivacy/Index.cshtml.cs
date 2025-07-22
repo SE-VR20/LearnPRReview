614c2b98d9980ddd775998d036dd9bef9d9934e8
varinder.rai <varinder.rai@netsmartz.com>
2025-07-22 18:03:33 +0530

    Added Privacy and the Home Page navigation links

---
+++ PRReviewProject/PRReviewProject/Pages/CustomPrivacy/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRReviewProject.Pages.Privacy
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
